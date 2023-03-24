ff931fc099a75ef0737baf7704fe0dff8d51c0cd
Mayuri Mahto <89357682+mayurimahto@users.noreply.github.com>
2023-03-24 15:59:47 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (55.28%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    //Using extra space
-    void recPermute(vector<int>&ds, int freq[], vector<vector<int>>&ans, vector<int>&nums){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+    //Method-2 Without using extra space
+    void recPermute(int ind, vector<vector<int>>&ans, vector<int>&nums){
+        if(ind==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        
-        for(int i=0; i<nums.size(); i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                recPermute(ds, freq, ans, nums);
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i=ind;i<nums.size(); i++){
+            swap(nums[ind], nums[i]);
+            recPermute(ind+1, ans, nums);
+            swap(nums[ind], nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        int freq[nums.size()];
-        for(int i=0; i<nums.size(); i++) freq[i] = 0;
-        recPermute(ds, freq, ans, nums);
+        recPermute(0, ans, nums);
         return ans;
     }
 };
