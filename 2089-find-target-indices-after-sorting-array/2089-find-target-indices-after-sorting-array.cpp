3cc5cbd2d63e5c8ac1bca70f56cf6b5df2a58bcb
Mayuri Mahto <89357682+mayurimahto@users.noreply.github.com>
2023-08-15 15:07:15 +0530

    Time: 2 ms (89.11%), Space: 11.6 MB (32.41%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,12 +1,54 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<int>ans;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == target){
-                ans.push_back(i);
+    int leftpoint(vector<int>&nums, int target, int n){
+        int low=0;
+        int high=n-1;
+        int res=-1;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                res = mid;
+                high = mid-1;
             }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return res;
+    }
+    int rightpoint(vector<int>&nums, int target, int n){
+        int low=0;
+        int high=n-1;
+        int res=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                res = mid;
+                low = mid+1;
+            }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return res;
+    }
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        int left = leftpoint(nums, target, n);
+        if(left == -1) return {};
+        int right = rightpoint(nums, target, n);
+        vector<int>ans;
+        
+        for(int i=left; i<=right; i++){
+            ans.push_back(i);
         }
         return ans;
     }
