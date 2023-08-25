4531106eb868eb143801c44445dacbf681cd1c3d
Mayuri Mahto <89357682+mayurimahto@users.noreply.github.com>
2023-08-25 18:28:30 +0530

    Time: 15 ms (98.13%), Space: 89.3 MB (76.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,26 +3,49 @@ public:
     double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
         //size of two given arrays:
         int n1 = a.size(), n2 = b.size();
+        int n = n1 + n2; //total size
+        //required indices:
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+        int cnt = 0;
+        int ind1el = -1, ind2el = -1;
 
-        vector<int> arr3;
         //apply the merge step:
         int i = 0, j = 0;
         while (i < n1 && j < n2) {
-            if (a[i] < b[j]) arr3.push_back(a[i++]);
-            else arr3.push_back(b[j++]);
+            if (a[i] < b[j]) {
+                if (cnt == ind1) ind1el = a[i];
+                if (cnt == ind2) ind2el = a[i];
+                cnt++;
+                i++;
+            }
+            else {
+                if (cnt == ind1) ind1el = b[j];
+                if (cnt == ind2) ind2el = b[j];
+                cnt++;
+                j++;
+            }
         }
 
         //copy the left-out elements:
-        while (i < n1) arr3.push_back(a[i++]);
-        while (j < n2) arr3.push_back(b[j++]);
-
-        //Find the median:
-        int n = n1 + n2;
-        if (n % 2 == 1) {
-            return (double)arr3[n / 2];
+        while (i < n1) {
+            if (cnt == ind1) ind1el = a[i];
+            if (cnt == ind2) ind2el = a[i];
+            cnt++;
+            i++;
+        }
+        while (j < n2) {
+            if (cnt == ind1) ind1el = b[j];
+            if (cnt == ind2) ind2el = b[j];
+            cnt++;
+            j++;
         }
 
-        double median = ((double)arr3[n / 2] + (double)arr3[(n / 2) - 1]) / 2.0;
-        return median;
+        //Find the median:
+        if (n % 2 == 1) {
+            return (double)ind2el;
+        }
+
+        return (double)((double)(ind1el + ind2el)) / 2.0;
     }
 };
